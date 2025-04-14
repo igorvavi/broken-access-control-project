495c77c04df182d0c6d43c45470125661a54c6e3
igorvavi <157329228+igorvavi@users.noreply.github.com>
2025-04-13 19:52:29 -0800

    Update app.py

--- app.py
+++ app.py
@@ -88,3 +88,10 @@ def adminify():
         user.role = 'admin'
         db.session.commit()
     return redirect(url_for('dashboard'))
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
